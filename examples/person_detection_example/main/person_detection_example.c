dd84451a37439fe6e8fcaa32a1e86a001755900a
Allison Chiang <83091926+allisonschiang@users.noreply.github.com>
2024-08-15 14:43:49 -0400

    changes

--- examples/person_detection_example/main/person_detection_example.c
+++ examples/person_detection_example/main/person_detection_example.c
@@ -8,11 +8,13 @@ void app_main(void)
 {
     float score = -0.1f;
     float *ptr = &score;
-
+    setup_model();
     if (get_person_score(ptr)){
         ESP_LOGI(TAG, "Score: %f", *ptr);
+        printf("success");
     } else {
         ESP_LOGE(TAG, "Score is -1.0f");
+        printf("fail");
     }
 
 }
